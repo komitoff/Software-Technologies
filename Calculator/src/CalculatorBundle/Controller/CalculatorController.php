47b4623304cbaca0520a5b2e7a8c3266980693bd
komitoff <tobo_91@abv.bg>
2017-03-12 15:19:16 +0200

    Simple calculator using symfony. 1. Implemented simple Calculator model. 2. Implemented simple logic for the calculations.

--- Calculator/src/CalculatorBundle/Controller/CalculatorController.php
+++ Calculator/src/CalculatorBundle/Controller/CalculatorController.php
@@ -7,6 +7,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use CalculatorBundle\Entity\Calculator;
 use CalculatorBundle\Form\CalculatorType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\DependencyInjection\Tests\Compiler\C;
 use Symfony\Component\HttpFoundation\Request;
 
 class CalculatorController extends Controller
@@ -21,6 +22,43 @@ class CalculatorController extends Controller
      */
     public function index(Request $request)
     {
-        return $this->render('calculator/index.html.twig');
+        $calculator = new Calculator();
+
+        $form = $this->createForm(CalculatorType::class, $calculator);
+        $form->handleRequest($request);
+        $result = 0;
+        if ($form->isValid() && $form->isSubmitted()) {
+            $leftOperand = $calculator->getLeftOperand();
+            $rightOperand = $calculator->getRightOperand();
+            $operator = $calculator->getOperator();
+
+            switch ($operator) {
+                case '+':
+                    $result = $leftOperand + $rightOperand;
+                    break;
+                case '-':
+                    $result = $leftOperand - $rightOperand;
+                    break;
+                case '*':
+                    $result = $leftOperand * $rightOperand;
+                    break;
+                case '/':
+                    $result = $leftOperand / $rightOperand;
+                    break;
+                case '%' :
+                    $result = $leftOperand % $rightOperand;
+                    break;
+                case '^' :
+                    $result = 1;
+                    for ($i = 0; $i < $rightOperand; $i ++ ) {
+                        $result *= $leftOperand;
+                    }
+                    break;
+                }
+
+        }
+
+        return $this->render('calculator/index.html.twig',
+            ['result' => $result, 'calculator' => $calculator, 'form' => $form->createView()]);
     }
 }
