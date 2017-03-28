31fdc1dd89cbf1d7ca28ecad8931a22cf9fa75d1
komitoff <tobo_91@abv.bg>
2017-03-28 20:25:05 +0300

    75/100 in add remove

--- JS exercises/10.add_remove.js
+++ JS exercises/10.add_remove.js
@@ -1,11 +1,35 @@
-function solve(args) {
-
-    let result = [];
-    for (let i = 0; i < args.length; i++) {
-        let tokens = args[i].split(' ');
-        let command = tokens[0];
-        if (command == 'add') {
-            result.add(tokens[1]);
+function addRemoveElements(command) {
+ 
+    let arr = [];
+ 
+    for (let i=0; i<command.length; i++) {
+        let temp = command[i].split(' ');
+        let cmd = temp[0];
+        let arg = temp[1];
+ 
+        switch (cmd) {
+            case 'add': arr.push(arg); break;
+            case 'remove':
+                let index = arg;
+                if (index == 0) {
+                    arr.shift();
+                    //console.log('Line ${i} shift - 0')
+                } else {
+                    if (index == arr.length - 1) {
+                        arr.splice();
+                        //console.log('Line ${i} pop - LAST')
+                    } else  {
+                        arr[index] = undefined;
+                    }
+                }
+                break;
+        }
+    }
+ 
+    for (let i=0; i<arr.length; i++)
+    {
+        if (arr[i] != undefined) {
+            console.log(arr[i]);
         }
     }
 }
