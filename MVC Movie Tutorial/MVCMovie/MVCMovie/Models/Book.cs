1279a67282769b86ecda06d0f06f5b1746fe3430
komitoff <tobo_91@abv.bg>
2017-04-03 15:27:52 +0300

    Playing with asp.net examples. Added Book model and BooksController

---
+++ MVC Movie Tutorial/MVCMovie/MVCMovie/Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCMovie.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+
+        public string Author { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        [StringLength(maximumLength:150, ErrorMessage = "Invalid Format", MinimumLength = 15)]
+        [DataType(DataType.Date)]
+        public DateTime dateReleased { get; set; }
+    }
+}
