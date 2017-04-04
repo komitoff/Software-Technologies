5d1175c8980652b9a7f7f8144483531eaf8ede4f
komitoff <tobo_91@abv.bg>
2017-04-04 19:42:55 +0300

    testing some simple operations with data structures in java.

--- JavaHomework/src/homework/CompareCharArray.java
+++ JavaHomework/src/homework/CompareCharArray.java
@@ -1,50 +1,29 @@
 package homework;
 
-import java.util.Collection;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class CompareCharArray {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
+        String[] firstLine = input.nextLine().split("\\s+");
+        String[] secondLine = input.nextLine().split("\\s+");
+        Set<String> sorted = new TreeSet<>();
+        sorted.add(stringify(firstLine));
+        sorted.add(stringify(secondLine));
 
-        String[] firstArr = scanner
-                .nextLine()
-                .split(" ");
-
-        String [] secondArr = scanner
-                .nextLine()
-                .split(" ");
-
-        System.out.println(firstArr);
-        //int minLen = Math.min(firstArr.length, secondArr.length);
-
-//        boolean flag = false;
-//
-//        for (int i = 0; i < minLen; i++) {
-//            if ((int)firstArr[i] < (int)secondArr[i]) {
-//                printArr(firstArr);
-//                printArr(secondArr);
-//                flag = true;
-//                break;
-//            }
-//
-//            if ((int)firstArr[i] > (int)secondArr[i]) {
-//                printArr(secondArr);
-//                printArr(firstArr);
-//                flag = true;
-//                break;
-//            }
-//        }
-//
-//        if(!flag) {
-//            printArr(firstArr);
-//            printArr(secondArr);
-//        }
+        for (String element : sorted) {
+            System.out.println(element);
+        }
     }
 
-    public static void printArr(char[] collection) {
-        for (char element : collection) {
-            System.out.print(element);
+    public static String stringify (String[] input) {
+        String output = "";
+        for (int i = 0; i < input.length; i ++) {
+            output = output.concat(input[i]);
         }
+        return output;
     }
 }
