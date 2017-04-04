b4bbd452adc9ce8ad23618f5010b59b447ab22a9
komitoff <tobo_91@abv.bg>
2017-04-04 20:27:20 +0300

    Implemented Calculator model and calculatorForm view.

--- Java Simple Calculator with Spring MVC/src/main/java/com/softuni/controller/HomeController.java
+++ Java Simple Calculator with Spring MVC/src/main/java/com/softuni/controller/HomeController.java
@@ -1,14 +1,28 @@
 package com.softuni.controller;
 
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
 	@GetMapping("/")
 	public String index(Model model) {
 		model.addAttribute("operator", "+");
+		model.addAttribute("view", "views/calculatorForm");
+		return "base-layout";
+	}
+
+	@PostMapping("/")
+	public String calculate(@RequestParam String leftOperand,
+							@RequestParam String rightOperand,
+							@RequestParam String operator,
+							Model model) {
+		model.addAttribute("view", "views/calculatorForm");
+
 		return "base-layout";
 	}
 }
