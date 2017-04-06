85f0fce6c0c1c13713c356b1b85a68db9ae25838
komitoff <tobo_91@abv.bg>
2017-04-06 20:31:04 +0300

    Added details view for specific article and method to call it.

--- Java Blog System/blog/src/main/java/softuniBlog/controller/HomeController.java
+++ Java Blog System/blog/src/main/java/softuniBlog/controller/HomeController.java
@@ -4,9 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import softuniBlog.bindingModel.ArticleBindingModel;
 import softuniBlog.entity.Article;
 import softuniBlog.repository.ArticleRepository;
 
+import javax.persistence.Id;
 import java.util.List;
 
 @Controller
@@ -20,6 +24,17 @@ public class HomeController {
         List<Article> articles = this.articleRepository.findAll();
         model.addAttribute("view", "home/index");
         model.addAttribute("articles", articles);
+        return "base-layout";
+    }
+
+    @GetMapping("/article/{id}")
+    public String details(Model model, @PathVariable Integer id) {
+        if(!this.articleRepository.exists(id)) {
+            return "redirect:/";
+        }
+        Article currentArticle = this.articleRepository.findOne(id);
+        model.addAttribute("view", "article/details");
+        model.addAttribute("article", currentArticle);
         return "base-layout";
     }
 
