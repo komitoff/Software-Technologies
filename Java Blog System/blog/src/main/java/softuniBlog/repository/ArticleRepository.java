a8b82ea45c751ca876528c56cda3b011799c167d
komitoff <tobo_91@abv.bg>
2017-04-06 19:10:08 +0300

    Added create view for the articles and get method in the article controller.

--- Java Blog System/blog/src/main/java/softuniBlog/repository/ArticleRepository.java
+++ Java Blog System/blog/src/main/java/softuniBlog/repository/ArticleRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import softuniBlog.entity.Article;
 
 public interface ArticleRepository extends JpaRepository<Article, Integer> {
-    
+
 }
 
