589dd7a9fc2cafb10fe3693770acb2b395357c4c
komitoff <tobo_91@abv.bg>
2017-04-14 13:04:21 +0300

    Added post action result take the submitted data and added calculate result.

--- C# Caclulator/Calculator-CSharp/Controllers/HomeController.cs
+++ C# Caclulator/Calculator-CSharp/Controllers/HomeController.cs
@@ -1,12 +1,48 @@
-﻿using System.Web.Mvc;
+﻿using Calculator_CSharp.Models;
+using System.Web.Mvc;
+using System;
 
 namespace Calculator_CSharp.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(Calculator calculator)
         {
-            return View();
+            return View(calculator);
+        }
+
+        [HttpPost]
+        public ActionResult Calculate(Calculator calculator)
+        {
+            calculator.Result = CalculateResult(calculator);
+
+            return RedirectToAction("index", calculator);
+        }
+
+        private decimal CalculateResult(Calculator calculator)
+        {
+            var result = 0m;
+
+            switch (calculator.Operator)
+            {
+                case "+":
+                    result = calculator.LeftOperand + calculator.RightOperand;
+                    break;
+                case "-":
+                    result = calculator.LeftOperand - calculator.RightOperand;
+                    break;
+                case "/":
+                    result = calculator.LeftOperand / calculator.RightOperand;
+                    break;
+                case "*":
+                    result = calculator.LeftOperand * calculator.RightOperand;
+                    break;
+                default:
+                    result = 0m;
+                    break;
+            }
+
+            return result;
         }
     }
 }
