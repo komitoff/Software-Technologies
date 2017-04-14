4f4127da6d79570872e29f21ff1742b77ea86b25
komitoff <tobo_91@abv.bg>
2017-04-14 12:55:04 +0300

    Built calculator skeleton and added calculator model.

---
+++ C# Caclulator/Calculator-CSharp/Models/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Calculator_CSharp.Models
+{
+    public class Calculator
+    {
+        public decimal LeftOperand { get; set; }
+        public decimal RightOperand { get; set; }
+        public String Operator { get; set; }
+        public decimal Result { get; set; }
+
+        public Calculator ()
+        {
+            this.Result = 0;
+        }
+    }
+}
